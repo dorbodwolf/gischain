f0875d48346768e1a47f84a4d0f6a7b699d3c3a7
pampa0629 <50463253@qq.com>
2023-10-11 20:12:36 +0800

    v0.0.3

--- dag.py
+++ dag.py
@@ -1,31 +1,35 @@
 import networkx as nx
 import matplotlib.pyplot as plt
 
+# 假设我们有以下任务和它们的输入输出关系
+tasks = [
+    {"id": "task1", "input": [], "output": ["A"]},
+    {"id": "task2", "input": ["A"], "output": ["B"]},
+    {"id": "task3", "input": ["B"], "output": ["C", "D"]},
+    {"id": "task4", "input": ["D"], "output": ["E"]},
+]
+
 # 创建一个有向图
 G = nx.DiGraph()
 
-# 定义任务列表，每个任务是一个元组 (任务名, 输入列表, 输出列表)
-tasks = [
-    ("TaskA", [], ["Data1"]),
-    ("TaskB", ["Data1"], ["Data2"]),
-    ("TaskC", ["Data1"], ["Data3"]),
-    ("TaskD", ["Data2", "Data3"], ["Data4"]),
-    ("TaskE", ["Data4"], ["Result"])
-]
-
-# 添加任务节点到图中
+# 添加任务和其输入输出到图中
 for task in tasks:
-    task_name, inputs, outputs = task
-    G.add_node(task_name, inputs=inputs, outputs=outputs)
+    task_id = task["id"]
+    G.add_node(task_id, color='lightblue', type='task')
+    
+    for inp in task["input"]:
+        G.add_node(inp, color='green', type='data')
+        G.add_edge(inp, task_id)
 
-# 添加有向边表示输入到输出的关系
-for task in tasks:
-    task_name, inputs, _ = task
-    for input_task in inputs:
-        G.add_edge(input_task, task_name)
+    for out in task["output"]:
+        G.add_node(out, color='red', type='data')
+        G.add_edge(task_id, out)
 
-# 绘制DAG图
-pos = nx.spring_layout(G, seed=42)  # 定义节点的布局
-nx.draw(G, pos, with_labels=True, node_size=800, node_color="lightblue", font_size=10, font_weight="bold")
-plt.title("DAG图")
+# 使用matplotlib进行绘图
+pos = nx.spring_layout(G)
+colors = [G.nodes[node]['color'] for node in G.nodes()]
+
+plt.figure(figsize=(10,6))
+nx.draw(G, pos, with_labels=True, node_color=colors, node_size=3000, edge_color='gray', width=1.5)
+plt.title('DAG depicting tasks and their inputs/outputs')
 plt.show()
