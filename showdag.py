6697509d9f8498f7f3969a7da79bd10d6c9ba606
pampa0629 <50463253@qq.com>
2023-10-12 21:14:27 +0800

    v0.0.4

--- showdag.py
+++ showdag.py
@@ -11,7 +11,6 @@ class ArrowedEdge(FancyArrowPatch):
         super(ArrowedEdge, self).__init__(posA, posB, connectionstyle="arc3,rad=0.", **kwargs)
 
 def draw_network_with_arrows(G, pos):
-    
     # 对每条边使用自定义的箭头
     for edge in G.edges():
         source, target = edge
@@ -20,29 +19,24 @@ def draw_network_with_arrows(G, pos):
         # 此行确保箭头与图的边界不会被裁剪
         plt.plot()
 
-# tasks 是一个List，里面的每个元素是一个字典，包括name、inputs和output三个key
-def showdag(tasks):
-
-    # 创建一个有向图
-    G = nx.DiGraph()
-
+# 从tasks中构造图
+def buildGaphic(G, tasks):
     # 添加任务和其输入输出到图中
     for task in tasks:
         task_name = task["name"]
-        # print(task_name)
         G.add_node(task_name, color='green', type='task')
         
         inputs = task["inputs"]
         for key,value in inputs.items():
-            # print(value)
             G.add_node(value, color='lightblue', type='data')
             G.add_edge(value, task_name)
             
         output = task["output"]
-        # print(output)
         G.add_node(output, color='red', type='data')
         G.add_edge(task_name, output)
 
+# 从图中提取颜色映射
+def get_color_mapping(G, tasks):
     # 创建颜色映射字典
     color_mapping = {
         "data": [],
@@ -53,11 +47,22 @@ def showdag(tasks):
         node_type = G.nodes[node]['type']
         color = G.nodes[node]['color']
         color_mapping[node_type].append(color)
+    return color_mapping
 
+# tasks 是一个List，里面的每个元素是一个字典，包括name、inputs和output三个key
+def showdag(tasks):
+
+    # 创建一个有向图
+    G = nx.DiGraph()
+
+    buildGaphic(G, tasks)
+    color_mapping = get_color_mapping(G, tasks)
+    
     plt.figure(figsize=(20,15))
 
     # 使用matplotlib进行绘图
-    pos = nx.spring_layout(G, k=1.0, iterations=100, seed=42, scale=2.0, center=(0, 0))
+    # seed 1 6 7 10
+    pos = nx.spring_layout(G, k=0.5, iterations=200, seed=7, scale=2.0, center=(0, 0))
 
     # data_colors = [G.nodes[node]['color'] for node in G.nodes()]
     # 提取不同类型的节点
@@ -66,14 +71,14 @@ def showdag(tasks):
 
     # 使用不同的绘制函数为不同类型的节点绘制形状
     nx.draw_networkx_nodes(G, pos, nodelist=data_nodes, node_shape="o",
-                        node_color=color_mapping["data"], node_size=2000)  # 圆形表示data
+                        node_color=color_mapping["data"], node_size=3000)  # 圆形表示data
     nx.draw_networkx_nodes(G, pos, nodelist=task_nodes, node_shape="h",
-                        node_color=color_mapping["task"], node_size=2000)  # 矩形表示task
+                        node_color=color_mapping["task"], node_size=3000)  # 矩形表示task
 
     nx.draw_networkx_labels(G, pos)
     # 定义箭头样式
     draw_network_with_arrows(G, pos)
 
-    plt.title('DAG depicting tasks and their inputs/outputs')
+    # plt.title('DAG depicting tasks and their inputs/outputs')
     plt.show()
     
---
+++ testtools.py
@@ -0,0 +1,25 @@
+# import tools
+from tools.buffer import buffer
+from tools.overlay import overlay
+from tools.slope import slope
+from tools.extractByValues import extractByValues
+from tools.extractByMask import extractByMask
+from tools.rasterOverlay import rasterOverlay
+from tools.polygon2mask import polygon2mask
+from tools.calculateArea import calculateArea
+
+# buffer("./data/railway.shp", 5000, "./data/buffer_railway.shp")
+# overlay("./data/buffer_railway.shp", "./data/land.shp", "./data/overlay_buffer_land.shp")
+
+# slope("./data/terrain.tif", "./data/slope_terrain.tif")
+# extractByValues("./data/slope_terrain.tif", 0, 10, "./data/slope_values.tif")
+
+# extractByValues("./data/terrain.tif", 0, 500, "./data/terrain_values.tif")
+
+# rasterOverlay("./data/slope_values.tif","./data/terrain_values.tif", "./data/overlay_slope_terrain.tif")
+
+# polygon2mask.polygon2mask("./data/overlay_buffer_land.shp", "./data/overlay_slope_terrain.tif", "./data/land_mask.tif")
+
+# extractByMask("./data/overlay_slope_terrain.tif","./data/overlay_buffer_land.shp", "./data/result_land.tif")
+
+print(calculateArea("./data/result_land.tif"))
